a0d0b52d295730a5f254e16e9f1d3c4c5f7cb592
ariomer <ariomer@gmail.com>
2021-02-28 08:47:09 +0000

    Second commit

--- src/Day01.java
+++ src/Day01.java
@@ -3,7 +3,9 @@ public class Day01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("11:05 - Birinci Gun");
+		System.out.println("11:57 - Birinci Gun");
+		System.out.println("Second Commit");
+		
 
 	}
 
