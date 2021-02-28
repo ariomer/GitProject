a6182955525e9a89145fb384966ef0485c35062e
Omer Ari <66365119+ariomer@users.noreply.github.com>
2021-02-28 11:43:43 +0000

    Update Day01.java

--- src/Day01.java
+++ src/Day01.java
@@ -8,6 +8,7 @@ public class Day01 {
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - Ikinci Gun");
 		System.out.println("10:35 - Ikinci Gun");
+		System.out.println("11:03 - Github.com");
 	}
 
 }
