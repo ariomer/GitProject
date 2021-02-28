b99035806ddd34a16b5b120c712fc4a4c753f849
ariomer <ariomer@gmail.com>
2021-02-28 13:08:43 +0000

    feature branch

--- src/Day01.java
+++ src/Day01.java
@@ -15,6 +15,7 @@ public class Day01 {
 		System.out.println("localden yeni satir");
 		System.out.println("localden 2.satir");
 		System.out.println("github yeni satir");
+		System.out.println("10:26 feature branch");
 	}
 
 }
