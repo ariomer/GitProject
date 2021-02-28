7f0e0e000653844ed503612ad54e715b6d85a112
ariomer <ariomer@gmail.com>
2021-02-28 14:10:14 +0000

    mesaj

--- src/Day01.java
+++ src/Day01.java
@@ -20,6 +20,7 @@ public class Day01 {
 		System.out.println("11:05 master branch");
 		System.out.println("11:20 feautre branch 3");
 		System.out.println("11:45 - feature branch");
+		//master
 	}
 
 }
