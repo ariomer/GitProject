69cdef009d78bd4ac9aa8be6eca7a9968e229dd8
ariomer <ariomer@gmail.com>
2021-02-28 13:51:10 +0000

    mesaj

--- src/Day01.java
+++ src/Day01.java
@@ -3,7 +3,7 @@ public class Day01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("11:57 - Birinci Gun");
+		System.out.println("Ben 12:00 da bu satiri feature branchte degistirdim");
 		System.out.println("Second Commit");
 		System.out.println("10:11 - Ikinci Gun");
 		System.out.println("10:26 - Ikinci Gun");
